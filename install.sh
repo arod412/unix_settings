a6099f5241992ba64a196562d8263f6e76ada0c8
Alexis Rodriguez Jr <a.rod19@gmail.com>
2020-01-28 13:15:04 -0500

    updated

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
+#!/bin/bash
+
 # Move all files to home directory
-mv .ssh ~
-mv .vim ~
-mv .bash_profile ~
-mv .bashrc ~
+
+cp -r .ssh ~
+cp -r .vim ~
 
 # Make current shell run new settings now
-. ~/.bash_profile
 
 # Install packages
 yum install -y screen
